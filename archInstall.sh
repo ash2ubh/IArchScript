44bb43e7e61b262ffc78bf423dd24b906191e0d8
ash2ubh <winoramaputra@protonmail.com>
2025-12-08 06:03:21 +0700

    modified:   archInstall.sh

--- archInstall.sh
+++ archInstall.sh
@@ -2,15 +2,15 @@
 
 echo -ne "Pre Installation"
 
-# source $CONFIGS_DIR/setup.conf
-# iso=$(curl -4 ifconfig.co/country-iso)
-# timedatectl set-ntp true
-# pacman -S --noconfirm archlinux-keyring 
-# pacman -S --noconfirm --needed pacman-contrib terminus-font
-# setfont ter-v22b
-# sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
-# pacman -S --noconfirm --needed reflector rsync grub
-# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+source $CONFIGS_DIR/setup.conf
+iso=$(curl -4 ifconfig.co/country-iso)
+timedatectl set-ntp true
+pacman -S --noconfirm archlinux-keyring 
+pacman -S --noconfirm --needed pacman-contrib
+setfont ter-132n
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacman -S --noconfirm --needed reflector rsync grub
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
 
-# reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
-# mkdir /mnt &>/dev/null
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+mkdir /mnt &>/dev/null
