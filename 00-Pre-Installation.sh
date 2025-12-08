1e04a2670f515b809da07f5c17f69e67a31bc8d9
ash2ubh <winoramaputra@protonmail.com>
2025-12-09 06:55:29 +0700

    new file:   00-Pre-Installation.sh

---
+++ 00-Pre-Installation.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+echo -ne "Pre Installation"
+
+source $CONFIGS_DIR/setup.conf
+iso=$(curl -4 ifconfig.co/country-iso)
+timedatectl set-ntp true
+pacman -S --noconfirm archlinux-keyring 
+pacman -S --noconfirm --needed pacman-contrib
+setfont ter-132n
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+pacman -S --noconfirm --needed reflector rsync grub
+cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+
+reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+mkdir /mnt &>/dev/null
