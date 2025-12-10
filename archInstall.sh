2d6124af98984629b7049eb6085205f9ff4725fe
ash2ubh <winoramaputra@protonmail.com>
2025-12-11 02:15:25 +0700

    modified:   archInstall.sh

--- archInstall.sh
+++ archInstall.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 bash curl https://archinstall.ash2ubh.xyz/01-Install.sh
-arch-chroot /mnt bash curl https://archinstall.ash2ubh.xyz/02-Chroot.sh ${1}
+bash arch-chroot /mnt curl https://archinstall.ash2ubh.xyz/02-Chroot.sh ${1}
 
 umount -IR /mnt
