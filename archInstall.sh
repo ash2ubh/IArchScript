4be45212cf4f5dac76465476a81bb9783606d567
ash2ubh <winoramaputra@protonmail.com>
2025-12-14 04:05:21 +0700

    deleted:    00-Pre-Installation.sh 	modified:   archInstall.sh

--- archInstall.sh
+++ archInstall.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-TARGET_DISK="$1"
-HOSTNAME="$2"
-USERNAME="$3"
-PASSWORD="$4"
-TIMEZONE="$5"
+TARGET_DISK=""
+HOSTNAME="$1"
+USERNAME="$2"
+PASSWORD="$3"
+TIMEZONE="$4"
 
 BOOT_PART="1"
-ROOT_PART="2"
-SWAP_PART="3"
+SWAP_PART="2"
+ROOT_PART="3"
 
 check_internet_connection(){
-    if ! ping -c 5 google.com &> /dev/null; then
+    if ! ping -c 5 archlinux.org  &> /dev/null; then
         echo -ne "Connect the internet with command:\n"
         echo -ne "- iwctl\n"
         echo -ne "- device list\n"
@@ -25,11 +25,169 @@ check_internet_connection(){
     fi
 }
 
+pre_setup(){
+    iso=$(curl -4 ifconfig.co/country-iso)
+    timedatectl set-ntp true
+
+    pacman -Sy 
+    pacman -S --noconfirm archlinux-keyring wget
+    pacman -S --noconfirm --needed pacman-contrib
+
+    sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+    pacman -S --noconfirm --needed reflector rsync grub
+    cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+    reflector -a 48 -c $iso -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+    mkdir /mnt &>/dev/null 
+}
+
+diskpart_setup(){
+    
+    list_disk=()
+    while IFS= read -r line; do
+        list_disk+=("$line")
+    
+    done < <(lsblk -d -o NAME,SIZE,TYPE,MODEL,MOUNTPOINT | grep -v '^$')
+
+    if [ ${#list_disk[@]} -eq 0 ]; then
+        echo "Disk Not Found!"
+        exit 1
+    fi
+
+    selected=1
+    quit=false
+
+    while ! $quit; do
+        clear
+        echo "#########################################################"
+        echo "#       Use arrow keys to select, and press Enter       #"
+        echo "#########################################################"
+        echo ""
+    
+        for i in "${!list_disk[@]}"; do
+            if [ $i -eq $selected ]; then
+                echo "> ${list_disk[$i]}"
+            else
+                echo "  ${list_disk[$i]}"
+            fi
+        done
+
+        read -rsn1 key
+        case "$key" in
+            $'\x1b')  
+                read -rsn2 -t 0.1 key2
+                case "$key2" in
+                    '[A') 
+                        selected=$((selected - 1))
+                        if [ $selected -lt 0 ]; then
+                            selected=$((${#list_disk[@]} - 1))
+                        fi
+                        ;;
+                    '[B') 
+                        selected=$((selected + 1))
+                        if [ $selected -ge ${#list_disk[@]} ]; then
+                            selected=0
+                        fi
+                        ;;
+                esac
+                ;;
+            '')  
+                quit=true
+                ;;
+            'q')  
+                exit 0
+                ;;
+        esac
+    done
+
+    selected_disk=$(echo "${list_disk[$selected]}" | awk '{print $1}')
+
+    TARGET_DISK="/dev/$selected_disk"
+
+    MEM_SIZE=$(awk '/MemTotal/ {print $2}' /proc/meminfo | numfmt --from-unit=K --to=si --suffix=B --format="%.2f" | awk '{printf "%d\n", $1}')
+
+    if [ "$MEM_SIZE" -gt 8 ]; then
+        SWAP_SIZE=$(awk '/MemTotal/ {print $2}' /proc/meminfo  | numfmt --from-unit=K --to=si --suffix=B --format="%.2f" | awk '{printf $1 * 1.5}' |awk '{printf "%dGB\n", $1}')
+    else
+        SWAP_SIZE="8GB"
+    fi
+    
+    echo "Swap SIZE [Auto Setup Used]: $SWAP_SIZE"
+    
+    read -p "Do you want used auto setup root partition based 50% capacity of the disk? [Disk Size must be more then 256GB] (y/n): " response
+
+    if [[ "$response" =~ ^[Yy]$|^[Yy][Ee][Ss]$ ]]; then
+        DISK_SIZE=$(lsblk -d -o NAME,SIZE | grep $selected_disk | awk '{printf $2}' | awk '{printf "%d\n", $1}')
+        echo "Total Disk Size: ${DISK_SIZE}GB"
+
+        if [ "$DISK_SIZE" -gt 256 ]; then
+            GET_SIZE=$((("$DISK_SIZE" - 1) / 2))
+            ROOT_SIZE=$((GET_SIZE -"${SWAP_SIZE%GB}"))"GB"
+        fi
+
+        echo "ROOT SIZE [Auto Setup Used]: $ROOT_SIZE"
+
+        fdisk $TARGET_DISK << EOF
+g
+n
+1
+
++1GB
+t
+1
+n
+2
+
++${SWAP_SIZE}
+t
+2
+19
+n
+3
+
++${ROOT_SIZE}
+t
+3
+20
+p
+w
+EOF
+
+    else
+        fdisk $TARGET_DISK << EOF
+g
+n
+1
+
++1GB
+t
+1
+n
+2
+
++${SWAP_SIZE}
+t
+2
+19
+n
+3
+
+t
+3
+20
+p
+w
+EOF
+
+    fi
+    
+    echo "Partitioning Successfully!"
+}
+
 format_partition(){
     echo "Start formatting partitions..."
 
-    mkfs.fat -F32 "${TARGET_DISK}${BOOT_PART}"
-    mkfs.ext4 "${TARGET_DISK}${ROOT_PART}" 
+    mkfs.fat -F32 -S 4096 "${TARGET_DISK}${BOOT_PART}"
+    mkfs.ext4 -b 4096 "${TARGET_DISK}${ROOT_PART}" 
 
     mkswap "${TARGET_DISK}${SWAP_PART}"
     swapon "${TARGET_DISK}${SWAP_PART}"
@@ -44,7 +202,7 @@ format_partition(){
 install_base(){
     echo "Start Installing base system..."
 
-    pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode nano sudo make cmake man-db man-pages texinfo bluez bluez-utils networkmanager network-manager-applet dhclient openssh cargo gcc pipewire alsa-utils pipewire-pulse pipewire-jack sof-firmware wget curl git glibc grub efibootmgr dosfstools mtools acpid --noconfirm --needed
+    pacstrap -K /mnt base base-devel linux-zen linux-zen-headers linux-firmware intel-ucode networkmanager wireless_tools wpa_supplicant bluez bluez-utils pipewire alsa-utils pipewire-pulse pipewire-jack sof-firmware dosfstools mtools util-linux exfatprogs e2fsprogs sudo nano man-db man-pages texinfo wget curl git grub efibootmgr os-prober --noconfirm --needed
 
     echo "Finished Installing base system."
 }
@@ -124,17 +282,25 @@ result_installation(){
     ###########################################################
     "
 
-    echo "Hostname: $HOSTNAME"
-    echo "Username: $USERNAME"
-    echo "Password: $PASSWORD"
-    echo "Timezone: $TIMEZONE"
-
+    echo -ne "
+    ###########################################################
+    #                 Hostname: $HOSTNAME                     #   
+    #                 Username: $USERNAME                     #
+    #                 Password: $PASSWORD                     #
+    #                 Timezone: $TIMEZONE                     #
+    ###########################################################
+    "
 }
 
 main(){
-    echo "Starting Arch Linux installation..."
+    
+    setfont ter-120n
 
+    echo "Starting Arch Linux installation..."
+    
     check_internet_connection
+    pre_setup
+    diskpart_setup
     format_partition
     install_base
     generate_fstab
